0ffdd7f80f4c5cddfc2ad1d4ea55911eacbfd024
zhangtao <511549108@qq.com>
2019-09-08 21:17:32 +0800

    添加登录页面

--- app/src/main/java/com/bangguo/app/common/utils/SettingSPUtils.java
+++ app/src/main/java/com/bangguo/app/common/utils/SettingSPUtils.java
@@ -23,17 +23,23 @@ import com.xuexiang.xutil.XUtil;
 import com.xuexiang.xutil.data.BaseSPUtil;
 
 /**
+ * SharedPreferences管理工具基类
+ *
  * @author xuexiang
- * @since 2018/7/16 下午3:38
+ * @since 2018/11/27 下午5:16
  */
 public class SettingSPUtils extends BaseSPUtil {
 
-    private static SettingSPUtils sInstance;
+    private static volatile SettingSPUtils sInstance = null;
 
     private SettingSPUtils(Context context) {
         super(context);
     }
 
+    /**
+     * 获取单例
+     * @return
+     */
     public static SettingSPUtils getInstance() {
         if (sInstance == null) {
             synchronized (SettingSPUtils.class) {
@@ -45,21 +51,22 @@ public class SettingSPUtils extends BaseSPUtil {
         return sInstance;
     }
 
+
+    private final String IS_FIRST_OPEN_KEY = "is_first_open_key";
+
     /**
-     * 获取服务器地址
-     *
-     * @return
+     * 是否是第一次启动
      */
-    public String getApiURL() {
-        return getString(getString(R.string.service_api_key), getString(R.string.default_service_api));
+    public boolean isFirstOpen() {
+        return getBoolean(IS_FIRST_OPEN_KEY, true);
     }
 
     /**
-     * 获取服务器地址
-     *
-     * @return
+     * 设置是否是第一次启动
      */
-    public boolean setApiURL(String apiUrl) {
-        return putString(getString(R.string.service_api_key), apiUrl);
+    public void setIsFirstOpen(boolean isFirstOpen) {
+        putBoolean(IS_FIRST_OPEN_KEY, isFirstOpen);
     }
+
+
 }
---
+++ app/src/main/java/com/bangguo/app/interfaces/OnDoIntListener.java
@@ -0,0 +1,9 @@
+package com.bangguo.app.interfaces;
+
+/**
+ * Created by LY on 2019/3/28.
+ * 动画工具类接口监听
+ */
+public interface OnDoIntListener {
+    void doSomething(int intValue);
+}
